c1358aa282bd7c6946de14617cc72e7bb1b50aa1
maclean <gilleain.torrance@gmail.com>
2012-10-12 23:03:42 +0100

    Fixed run script

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 CDK_DIR=../../cdk/cdk
-DIST={$CDK_DIR}/dist/jar
-CLASSPATH=${DIST}/*:${CDK_DIR}:${CDK_DIR}/jar/*:bin:lib/*
+DIST=${CDK_DIR}/dist/jar
+CLASSPATH=${DIST}/*:${CDK_DIR}/jar/*:bin:lib/*
 java -cp ${CLASSPATH} app.AMG $*
