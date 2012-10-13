649c13b5522c416dbba48f1a574c95f1706162d2
maclean <gilleain.torrance@gmail.com>
2012-10-13 17:06:09 +0100

    Alkene tests

---
+++ src/test/generate/symmetry/AlkeneTests.java
@@ -0,0 +1,39 @@
+package test.generate.symmetry;
+
+import generate.AtomAugmentingGenerator.ListerMethod;
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import test.generate.BaseTest;
+
+public class AlkeneTests extends BaseTest {
+    
+    public static final ListerMethod METHOD = ListerMethod.SYMMETRIC;
+    
+    @Test
+    public void c2H4Test() {
+        Assert.assertEquals(1, countNFromSingleDoubleTriple("C2H4", METHOD));
+    }
+    
+    @Test
+    public void c3H6Test() {
+        Assert.assertEquals(2, countNFromSingleDoubleTriple("C3H6", METHOD));
+    }
+    
+    @Test
+    public void c4H8Test() {
+        Assert.assertEquals(5, countNFromSingleDoubleTriple("C4H8", METHOD));
+    }
+    
+    @Test
+    public void c5H10Test() {
+        Assert.assertEquals(10, countNFromSingleDoubleTriple("C5H10", METHOD));
+    }
+    
+    @Test
+    public void c6H12Test() {
+        Assert.assertEquals(25, countNFromSingleDoubleTriple("C6H12", METHOD));
+    }
+
+}
