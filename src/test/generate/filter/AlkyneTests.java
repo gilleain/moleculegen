881941d52552502145242b9624c57fbfb91eebed
maclean <gilleain.torrance@gmail.com>
2012-10-13 17:53:33 +0100

    Alkyne tests

---
+++ src/test/generate/filter/AlkyneTests.java
@@ -0,0 +1,39 @@
+package test.generate.filter;
+
+import generate.AtomAugmentingGenerator.ListerMethod;
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import test.generate.BaseTest;
+
+public class AlkyneTests extends BaseTest {
+    
+    public static final ListerMethod METHOD = ListerMethod.SYMMETRIC;
+    
+    @Test
+    public void c2H2Test() {
+        Assert.assertEquals(1, countNFromAtom("C2H2", METHOD));
+    }
+    
+    @Test
+    public void c3H4Test() {
+        Assert.assertEquals(3, countNFromAtom("C3H4", METHOD));
+    }
+    
+    @Test
+    public void c4H6Test() {
+        Assert.assertEquals(9, countNFromAtom("C4H6", METHOD));
+    }
+    
+    @Test
+    public void c5H8Test() {
+        Assert.assertEquals(26, countNFromAtom("C5H8", METHOD));
+    }
+    
+    @Test
+    public void c6H10Test() {
+        Assert.assertEquals(77, countNFromAtom("C6H10", METHOD));
+    }
+
+}
