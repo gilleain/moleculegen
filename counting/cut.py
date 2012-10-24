5ab97bc1ab5b836728dc922e686f0197f12e0630
maclean <gilleain.torrance@gmail.com>
2012-10-24 14:26:05 +0100

    Tidy away the accounting data into its own directory

---
+++ counting/cut.py
@@ -0,0 +1,15 @@
+import sys
+
+line_counter = 0
+max_count = int(sys.argv[2])
+for line in open(sys.argv[1]):
+    line_counter += 1
+    if line_counter == 1: 
+        #for i, bit in enumerate(line.split()): print i, bit
+        continue 
+    line = line.strip()
+    bits = line.split()
+    n1 = int("".join(bits[4].split(',')))
+    n2 = int("".join(bits[11].split(',')))
+    print "\t".join([str(x) for x in [n1, n2, bits[3]]])
+    if max_count > 0 and line_counter > max_count: break
