76dae6e1929d77db929bb995d93356c9d0f9b08c
maclean <gilleain.torrance@gmail.com>
2015-01-09 19:03:18 +0000

    Alter paths in shell scripts, but there should be a better way...

--- diff.sh
+++ diff.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-CDK_DIR=../../cdk/cdk
-DIST=${CDK_DIR}/dist/jar
-CLASSPATH=${DIST}/*:${CDK_DIR}/jar/*:bin:lib/*
+CDK_DIR=../../chemistry/cdk
+CLASSPATH=bin:lib/*
+for p in ${CDK_DIR}/*/*/target/classes; do
+	CLASSPATH="${CLASSPATH}:${p}"
+done
+CLASSPATH=${DIST}/*:$CLASSPATH:/Users/maclean/.m2/repository/java3d/vecmath/1.5.2/vecmath-1.5.2.jar
+CLASSPATH=$CLASSPATH:/Users/maclean/.m2/repository/uk/ac/ebi/beam/beam-core/0.8/beam-core-0.8.jar
+CLASSPATH=$CLASSPATH:/Users/maclean/.m2/repository/uk/ac/ebi/beam/beam-func/0.8/beam-func-0.8.jar
+CLASSPATH=$CLASSPATH:/Users/maclean/.m2/repository/com/google/collections/google-collections/1.0/google-collections-1.0.jar
 java -cp ${CLASSPATH} app.DiffTool $*
