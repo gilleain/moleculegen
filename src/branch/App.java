8df40b5e23629db140e51ef2bcba630a901ef9d1
maclean <gilleain.torrance@gmail.com>
2015-05-26 21:08:44 +0100

    Timings in the app, and a failing pair of nines...

--- src/branch/App.java
+++ src/branch/App.java
@@ -1,30 +1,70 @@
 package branch;
 
+import java.util.Stack;
+
 public class App {
     
-    public static void count(String formula) {
+    private static long start(boolean shouldTime) {
+        if (shouldTime) {
+            return System.nanoTime();
+        } else {
+            return 0;
+        }
+    }
+    
+    private static void time(long start, boolean shouldTime) {
+        if (shouldTime) {
+            long timeInNs = (System.nanoTime() - start);
+            long timeInMs = timeInNs / 1000000;
+            long timeInS = timeInNs / 1000000000;
+            System.out.println("Time(ms) : [" + timeInMs + "]");
+            System.out.println("Time(s) : [" + timeInS + "]");
+        }
+    }
+    
+    public static void count(String formula, boolean shouldTime) {
+        long start = start(shouldTime);
         CountingHandler handler = new CountingHandler();
         AtomGenerator generator = new AtomGenerator(formula, handler);
         generator.run();
         System.out.println(handler.getCount());
+        time(start, shouldTime);
     }
     
-    public static void print(String formula, boolean showCount) {
+    public static void print(String formula, boolean showCount, boolean shouldTime) {
+        long start = start(shouldTime);
         PrintStreamHandler handler = new PrintStreamHandler(System.out, showCount);
         AtomGenerator generator = new AtomGenerator(formula, handler);
         generator.run();
+        time(start, shouldTime);
     }
     
     public static void main(String[] args) {
-        String formula = args[0];
-        if (args.length > 1 && args[1].equals("-p")) {
-            if (args.length > 2 && args[2].equals("-n")) {
-                print(formula, true);
-            } else {
-                print(formula, false);
+        Stack<String> argStack = new Stack<String>();
+        for (String arg : args) {
+            argStack.add(arg);
+        }
+        String formula = argStack.pop();
+        
+        boolean shouldPrint = false;
+        boolean shouldNumber = false;
+        boolean shouldTime = false;
+        
+        while (argStack.size() > 0) {
+            String arg = argStack.pop();
+            if (arg.equals("-p")) {
+                shouldPrint = true;
+            } else if (args.equals("-n")) {
+                shouldNumber = true;
+            } else if (arg.equals("-t")) {
+                shouldTime = true;
             }
+        }
+        
+        if (shouldPrint) {
+            print(formula, shouldNumber, shouldTime);
         } else {
-            count(formula);
+            count(formula, shouldTime);
         }
     }
 
--- src/test/branch/TestAtomGenerator.java
+++ src/test/branch/TestAtomGenerator.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
-import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IChemObjectBuilder;
 import org.openscience.cdk.silent.SilentChemObjectBuilder;
