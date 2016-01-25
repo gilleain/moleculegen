184ec27db548a996476abe99dbc2468512cdea6a
maclean <gilleain.torrance@gmail.com>
2016-01-25 12:38:11 +0000

    Duplicate the atom augmentor to try out constraints

---
+++ src/augment/atomconstrained/ElementSymbolSource.java
@@ -0,0 +1,19 @@
+package augment.atomconstrained;
+
+import app.FormulaParser;
+import augment.ExtensionSource;
+
+public class ElementSymbolSource implements ExtensionSource<Integer, String> {
+    
+    private final FormulaParser formulaParser;
+    
+    public ElementSymbolSource(FormulaParser formulaParser) {
+        this.formulaParser = formulaParser;
+    }
+
+    @Override
+    public String getNext(Integer position) {
+        return formulaParser.getElementSymbols().get(position);
+    }
+
+}
